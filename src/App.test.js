c633432fadfe5f23676204cd74589f752a62b1ea
Toughee <Jimernez.io@gmail.com>
2021-12-30 22:08:01 -0600

    FontsHub/001/Initial_App_Update
    - Added React front end into project
    - Added documentation into project
    - Added CI/CD scripts into project
    - Initialized UI design for app

---
+++ src/App.test.js
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react'
+import App from './App'
+
+test('renders front end page with no error message', () => {
+    render(<App />)
+})
---
+++ src/components/Font7.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default class Font7 extends React.Component {
+    render() {
+        return (
+            <div className="fontsContainer">
+                <h1 className="mainFont">Font 7</h1>
+
+                <p className="fontDesc">
+                    About font 7: cies lobortis. Mauris leo ante, blandit sit
+                    amet malesuada quis, ultrices eu ex. Vestibulum vestibulum
+                    ligula nunc, quis faucibus neque molestie eget. Cras gravida
+                    semper hendrerit. Praesent ac metus in est cursus ultricies.
+                    Cras eu pulvinar lectus, eu molestie mauris. In imperdiet,
+                    tellus vel consequat commo
+                </p>
+
+                <h1>h1: A B C | a b c</h1>
+
+                <h2>h2: A B C | a b c</h2>
+
+                <h3>h3: A B C | a b c</h3>
+
+                <h4>h4: A B C | a b c</h4>
+                <h5>h5: A B C | a b c</h5>
+                <h6>h6: A B C | a b c</h6>
+
+                <p>p: A B C | a b c</p>
+            </div>
+        )
+    }
+}
