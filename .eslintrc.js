1fa5974f15d4bc191ceabc4e6e79ef06298a78f3
Sergio Blackhelm DeLeón <codework@Sergios-MBP.lan>
2023-01-11 15:12:28 -0600

    FontsPanel/019/Dependencies_Update_07
    - Updated node modules and yarn packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,21 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended',
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
     ],
-    overrides: [],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
     },
-    plugins: ['react', '@typescript-eslint'],
-    rules: {},
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
 }
