1a6831eac577a837c819e25df62a841a0229d708
Sergio Blackhelm DeLeón <codework@Sergios-MBP.lan>
2023-02-02 23:22:12 -0600

    FontsPanel/020/Dependencies_Update_08
    - Updated node modules and yarn packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,14 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es2021: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
     },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+    plugins: ['react'],
+    rules: {},
 }
--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,13 @@
 module.exports = {
     presets: [
-      'babel-preset-react-app',
-      [ 
         'babel-preset-react-app',
-        {
-          targets: {
-            node: 'current',
-          },
-        },
-      ],
+        [
+            'babel-preset-react-app',
+            {
+                targets: {
+                    node: 'current',
+                },
+            },
+        ],
     ],
-  };
+}
